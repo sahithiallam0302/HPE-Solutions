dc8a29d001aeafc635a3d5346d90c79f43d985a7
BalajiRekha3 <rekhabalaji37@gmail.com>
2026-02-27 17:55:17 +0530

    Home page inside cover and sections added

--- HPE_IT_Solutions/src/pages/Home.jsx
+++ HPE_IT_Solutions/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Hero from '../components/Hero';
 import AboutUsSection from '../components/HomePage-Sections/AboutUsSection';
 import OurDivisions from '../components/HomePage-Sections/OurDivisions';
 import Certifications from '../components/Certifications';
+import WorkforceStrength from '../components/HomePage-Sections/WorkforceStrength';
+import ImpactAtScale from '../components/HomePage-Sections/ImpactAtScale';
+import ContactCTA from '../components/HomePage-Sections/ContactCTA';
 const Home = () => {
     return (
         <>
@@ -10,6 +13,9 @@ const Home = () => {
             <AboutUsSection />
             <OurDivisions />
             <Certifications />
+            <WorkforceStrength />
+            <ImpactAtScale />
+            <ContactCTA />
         </>
     );
 };
